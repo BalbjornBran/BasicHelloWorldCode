94bf58a25eaeed8a89803089e3b99abe5836ba9d
BalbjornBran <tony.hammet@gmail.com>
2025-01-17 21:19:34 +0000

    First Commit with the basic program
    This is the first commit, just adding a hello world log on the compiler. I'm using Visual Studio Code for this repository.

---
+++ Hello.cpp
@@ -0,0 +1,20 @@
+/* 
+    Copyright: Balbjorn Bran
+    Hello.cpp is a basic program as an entry point for learning c++, 
+    we're gonna be learnig how to print strings, inside the main function using the C standar I-O library. 
+    We're gonna be using single and multi-line comments to organize the code.
+*/
+
+//First, we have to define the libraries that we are going to use in the program to be able to access its functions.
+#include <cstdio>
+
+//And to start, we need the main function, which is the following:
+int main()
+{
+
+    // We call the printf function available in the c standard library I.O.(Input, Output) to display a message on the screen
+    printf("Hello, my name is Balbjorn.\n");
+    // We can use the character \n to jump to a different line 
+    printf("This file is ready to run.\n");
+    
+}
